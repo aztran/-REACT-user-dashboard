c2917f1ebafbc027225c95a8f54ad9041dfa806e
aztran <andra.riztyan@gmail.com>
2019-02-22 00:14:09 +0700

    :hammer: add Header & Menu based on antdesign

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+import { Menu, Icon } from 'antd';
+import MenuItem from './MenuItem/MenuItem';
+
+
+
+class Header extends Component {
+  state = {
+    current: 'mail',
+  }
+
+  handleClick = (e) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <div className="header-wrapper--blue">
+          <div className="header__top">
+            <h3 class="header__title">User</h3>
+            <h3 class="header__title">Dashboard</h3>
+          </div>
+        </div>
+        <div className="header-wrapper">
+          <div className="header__bottom">
+            <MenuItem />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
